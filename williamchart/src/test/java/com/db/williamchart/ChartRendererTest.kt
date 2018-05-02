8a8e63df184703a66c6851bae522b799552d7c6c
diogobernardino <diogofilipe.b@gmail.com>
2018-05-03 00:48:54 +0900

    Make tests work w/ help of third party libs.

--- williamchart/src/test/java/com/db/williamchart/ChartRendererTest.kt
+++ williamchart/src/test/java/com/db/williamchart/ChartRendererTest.kt
@@ -1,26 +1,37 @@
 package com.db.williamchart
 
+import com.db.williamchart.data.ChartSet
 import com.db.williamchart.data.Line
 import com.db.williamchart.data.Point
 import com.db.williamchart.renderer.ChartRenderer
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
 
 class ChartRendererTest {
 
+    @Mock private lateinit var view: ChartContract.View
+
     @Mock private lateinit var painter: Painter
+
+    @Captor private lateinit var setCaptor: ArgumentCaptor<ChartSet>
 
     private lateinit var renderer: ChartRenderer
 
     @Before fun setup() {
+
         MockitoAnnotations.initMocks(this)
-        renderer = ChartRenderer(painter)
+        renderer = ChartRenderer(view, painter)
     }
 
     @Test(expected = IllegalArgumentException::class)
@@ -29,15 +40,16 @@ class ChartRendererTest {
         val set = Line()
         set.add(Point("label", 1f))
 
-        renderer.data = set
-
+        renderer.add(set)
         renderer.preDraw(0, 0, 0, 0, 0, 0)
     }
 
     @Test
     fun noData_SkipPreDraw() {
 
-        renderer.preDraw(0, 0, 0, 0, 0, 0)
+        renderer.draw()
+
+        verify(view, times(0)).drawData(any(), any(), any(), any(), any())
     }
 
     @Test
@@ -47,13 +59,19 @@ class ChartRendererTest {
         set.add(Point("label0", 0f))
         set.add(Point("label1", 1f))
 
-        renderer.data = set
-
+        renderer.add(set)
         renderer.preDraw(0, 1, 0, 0, 0, 0)
+        renderer.draw()
 
-        val result = renderer.data
-        assertEquals(1F, result!!.entries[0].y)
-        assertEquals(0F, result.entries[1].y)
+        verify(view).drawData(any(), any(), any(), any(), capture(setCaptor))
+        assertEquals(1F, setCaptor.value.entries[0].y)
+        assertEquals(0F, setCaptor.value.entries[1].y)
     }
 
 }
+
+/**
+ * Returns ArgumentCaptor.capture() as nullable type to avoid java.lang.IllegalStateException
+ * when null is returned.
+ */
+fun <T> capture(argumentCaptor: ArgumentCaptor<T>): T = argumentCaptor.capture()

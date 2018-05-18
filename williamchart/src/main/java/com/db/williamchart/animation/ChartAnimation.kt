fea4f471fb41cafa918ff38bfc43dc1e6f6ebabf
diogobernardino <diogofilipe.b@gmail.com>
2018-05-18 12:27:47 +0900

    Configure custom animation interpolator.

--- williamchart/src/main/java/com/db/williamchart/animation/ChartAnimation.kt
+++ williamchart/src/main/java/com/db/williamchart/animation/ChartAnimation.kt
@@ -1,10 +1,14 @@
 package com.db.williamchart.animation
 
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.Interpolator
 import com.db.williamchart.data.ChartEntry
 
 abstract class ChartAnimation {
 
     var duration: Long = 1000
+
+    var interpolator: Interpolator = DecelerateInterpolator()
 
     abstract fun animateFrom(y: Float, entries: MutableList<ChartEntry>, callback: () -> Unit) : ChartAnimation
 }
--- williamchart/src/main/java/com/db/williamchart/animation/DefaultAnimation.kt
+++ williamchart/src/main/java/com/db/williamchart/animation/DefaultAnimation.kt
@@ -14,12 +14,14 @@ class DefaultAnimation : ChartAnimation() {
         entries.forEach { entry ->
             val eAnimator: ObjectAnimator = ObjectAnimator.ofFloat(entry, "y", y, entry.y)
             eAnimator.duration = duration
+            eAnimator.interpolator = interpolator
             eAnimator.start()
         }
 
         val animator: ValueAnimator = ValueAnimator.ofInt(0,1)
         animator.addUpdateListener { callback.invoke() }
         animator.duration = duration
+        animator.interpolator = interpolator
         animator.start()
 
         return this

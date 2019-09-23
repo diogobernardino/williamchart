fd21b118637ebea2d60df14f61eaa5b56190a43c
Diogo Bernardino <diogofilipe.b@gmail.com>
2019-09-23 08:46:15 +0200

    Remove tests related to multiple preDraw calls since this is now allowed in order to update data.

--- williamchart/src/test/java/com/db/williamchart/renderer/LineChartRendererTest.kt
+++ williamchart/src/test/java/com/db/williamchart/renderer/LineChartRendererTest.kt
@@ -9,7 +9,6 @@ import com.db.williamchart.data.Paddings
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class LineChartRendererTest {
@@ -24,57 +23,6 @@ class LineChartRendererTest {
             painter = painter,
             animation = animation
         )
-    }
-
-    @Test(expected = IllegalArgumentException::class)
-    fun `at least 2 datapoints are needed to display chart`() {
-        // Arrange
-        val emptyData = linkedMapOf<String, Float>()
-        val chartConfiguration = LineChartConfiguration(
-            width = 0,
-            height = 0,
-            paddings = Paddings(0f, 0f, 0f, 0f),
-            axis = AxisType.NONE,
-            labelsSize = 0f,
-            lineThickness = 0f,
-            pointsDrawableWidth = 0,
-            pointsDrawableHeight = 0,
-            fillColor = 0,
-            gradientFillColors = intArrayOf()
-        )
-
-        // Act
-        lineChartRenderer.render(emptyData)
-        lineChartRenderer.preDraw(chartConfiguration)
-    }
-
-    @Test
-    fun `predraw must execute only once`() {
-        // Arrange
-        val data = linkedMapOf(
-            "this" to 999f,
-            "that" to 111f
-        )
-        val chartConfiguration = LineChartConfiguration(
-            width = 0,
-            height = 0,
-            paddings = Paddings(0f, 0f, 0f, 0f),
-            axis = AxisType.NONE,
-            labelsSize = 0f,
-            lineThickness = 0f,
-            pointsDrawableWidth = 0,
-            pointsDrawableHeight = 0,
-            fillColor = 0,
-            gradientFillColors = intArrayOf()
-        )
-
-        // Act
-        lineChartRenderer.render(data)
-        lineChartRenderer.preDraw(chartConfiguration)
-        val wasPreparedAlready = lineChartRenderer.preDraw(chartConfiguration)
-
-        // Assert
-        assertTrue(wasPreparedAlready)
     }
 
     @Test

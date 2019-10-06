f04e07bd40dd2034d38abb68129c37ccd32ea09c
Diogo Bernardino <diogofilipe.b@gmail.com>
2019-10-06 19:29:32 +0200

    First successful draw.

--- williamchart/src/main/java/com/db/williamchart/view/PieChartView.kt
+++ williamchart/src/main/java/com/db/williamchart/view/PieChartView.kt
@@ -1,37 +1,31 @@
 package com.db.williamchart.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.widget.FrameLayout
 import androidx.core.graphics.toRectF
-import com.db.williamchart.ChartContract
-import com.db.williamchart.data.DataPoint
 import com.db.williamchart.data.Frame
-import com.db.williamchart.data.Label
 import com.db.williamchart.data.toRect
 
 class PieChartView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ChartView(context, attrs, defStyleAttr), ChartContract.View {
+) : FrameLayout(context, attrs, defStyleAttr) {
 
-    @Suppress("MemberVisibilityCanBePrivate")
-    var innerCircleRadius = 0f
+    private val paint: Paint = Paint()
 
-    override fun drawLabels(xLabels: List<Label>) {
-    }
-
-    override fun drawData(innerFrame: Frame, entries: List<DataPoint>) {
-        canvas.drawCircle(CIRCLE_CENTER, CIRCLE_CENTER, CIRCLE_RADIUS, painter.paint)
-        canvas.drawArc(innerFrame.toRect().toRectF(), START_ANGLE, CURRENT_ANGLE, false, painter.paint)
-    }
-
-    override fun drawDebugFrame(outerFrame: Frame, innerFrame: Frame, labelsFrame: List<Frame>) {
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 20f
+        val frame = Frame(0f, 0f, width.toFloat(), height.toFloat())
+        canvas?.drawArc(frame.toRect().toRectF(), START_ANGLE, CURRENT_ANGLE, false, paint)
     }
 
     companion object {
-        private const val CIRCLE_CENTER = 50f
-        private const val CIRCLE_RADIUS = 20f
         private const val START_ANGLE = 90f
         private const val CURRENT_ANGLE = 40f
     }

2049d29bd194af28ad3819bce4e546af3a4c179c
DarkMatterMatt <mattmoran2000@gmail.com>
2019-10-21 20:37:45 +1300

    Removed unused import

--- mobile/src/main/java/com/db/williamchartdemo/DemoFragment.kt
+++ mobile/src/main/java/com/db/williamchartdemo/DemoFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.db.williamchart.data.Scale
 import kotlinx.android.synthetic.main.demo_fragment.*
 
 class DemoFragment : Fragment() {

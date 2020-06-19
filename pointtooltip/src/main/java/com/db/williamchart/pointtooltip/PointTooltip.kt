13e2d5c483f36384df26a51adf067f9c1ae43239
Diogo Bernardino <diogofilipe.b@gmail.com>
2020-06-20 00:04:42 +0200

    Create tooltip class extending tooltip.

---
+++ pointtooltip/src/main/java/com/db/williamchart/pointtooltip/PointTooltip.kt
@@ -0,0 +1,14 @@
+package com.db.williamchart.pointtooltip
+
+import android.view.ViewGroup
+import com.db.williamchart.Tooltip
+
+class PointTooltip : Tooltip {
+    override fun onCreateTooltip(parentView: ViewGroup) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDataPointTouch(x: Float, y: Float) {
+        TODO("Not yet implemented")
+    }
+}

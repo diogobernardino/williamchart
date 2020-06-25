cf9446576b699631a4436273a55f6fbefa3897db
Diogo Bernardino <diogofilipe.b@gmail.com>
2020-06-25 15:45:41 +0200

    Creat new grid module with FullGrid component.

---
+++ grid/src/main/java/com/db/williamchart/grid/FullGrid.kt
@@ -0,0 +1,8 @@
+package com.db.williamchart.grid
+
+import android.view.ViewGroup
+import com.db.williamchart.Grid
+
+class FullGrid : Grid {
+    override fun onCreateTooltip(parentView: ViewGroup) {}
+}

99edbc0415a003a902ae5b3a9ef6418d1d7e047b
Diogo Bernardino <diogofilipe.b@gmail.com>
2020-11-13 00:29:52 +0100

    Use labels plugin in horizontal bar chart.

--- williamchart/src/main/java/com/db/williamchart/view/HorizontalBarChartView.kt
+++ williamchart/src/main/java/com/db/williamchart/view/HorizontalBarChartView.kt
@@ -88,21 +88,8 @@ class HorizontalBarChartView @JvmOverloads constructor(
     }
 
     override fun drawLabels(xLabels: List<Label>) {
-
-        painter.prepare(
-            textSize = labelsSize,
-            color = labelsColor,
-            font = labelsFont
-        )
-
-        xLabels.forEach {
-            canvas.drawText(
-                it.label,
-                it.screenPositionX,
-                it.screenPositionY,
-                painter.paint
-            )
-        }
+        painter.prepare(textSize = labelsSize, color = labelsColor, font = labelsFont)
+        labels.draw(canvas, painter.paint, xLabels)
     }
 
     override fun drawDebugFrame(frames: List<Frame>) {
